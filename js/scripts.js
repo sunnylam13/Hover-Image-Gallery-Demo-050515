16e85863be7febb6c60900b87fe67e0cb7d78488
Sunny Lam <sunny.lam@gmail.com>
2015-05-07 13:56:48 -0400

    // NOTE/WARNING:  removing the css transform on the image holding divs div.vlak_0, div.vlak_1 prevents the sideways translate in my animation If you apply a translation to the individual images instead of targeting the div.scene that holds the images, the animations on the images mysteriously DO NOT work. I have yet to figure this out.

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,32 @@ jQuery(document).ready(function($) {
 // GLOBAL VARIABLES
 
 // create a new TimelineMax sequencing object
-var tm = new TimelineMax({repeat:-1,yoyo:true});
+var tm1 = new TimelineMax({repeat:-1,yoyo:true});
+// var tm1 = new TimelineMax({repeat:-1});
 
 //////////////////////////////////////////////////
 // FUNCTIONS
 
-function hoverAnimateImage1 (targetE) {
+function hoverAnimateImage1 (targetE,tmObj) {
 	// where targetE = the target element, typically a jQuery selector
 	
+	console.log('Hover animation 1 initiated...');
+
 	// store a reference to the target
 	var $target = $(targetE);
 
-	tm.yoyo(true);
-	tm.staggerTo($target,2,{css:{transform:'translateZ(5px)'}, ease:Power0.easeOut},1);
-	tm.staggerTo($target,2,{css:{transform:'translateZ(0px)'}, ease:Power0.easeOut},1);
-	// tm.staggerTo($target,0.5,{translateX:5, ease:Power2.easeIn, overwrite:'all'},0.6);
+	tmObj.yoyo(true);
+	// tmObj.staggerTo($target,1,{css:{transform:'translateZ(32px)'}, ease:Power0.easeOut},0.2);
+	// tmObj.staggerTo($target,1,{css:{transform:'translateZ(0px)'}, ease:Power0.easeOut},0.2);
+	// tmObj.staggerTo($target,2,{css:{transform:'translateZ(32px)'}, ease:Power0.easeOut},1);
+	// tmObj.staggerTo($target,2,{css:{transform:'translateZ(0px)'}, ease:Power0.easeOut},1);
+	// add the other dimensions and keep them at 0
+	// tmObj.staggerTo($target,1,{css:{transform:'translateX(0) translateY(0) translateZ(15px)'}, ease:Power0.easeOut},0.2);
+	// tmObj.staggerTo($target,1,{css:{transform:'translateX(0) translateY(0) translateZ(0px)'}, ease:Power0.easeOut},0.1);
+	tmObj.staggerTo($target,1.6,{css:{transform:'translateX(0) translateY(0) translateZ(15px)'}, ease:SlowMo.easeOut},0.2);
+	tmObj.staggerTo($target,1.6,{css:{transform:'translateX(0) translateY(0) translateZ(0px)'}, ease:SlowMo.easeOut},0.2);
 
-	// tm.to($target,0.5,{rotateX:180, z:-300, ease:Power2.easeIn, overwrite:'all'});
-
-	tm.play();
+	tmObj.play();
 
 }
 
@@ -30,8 +37,11 @@ function hoverAnimateImage1 (targetE) {
 //////////////////////////////////////////////////
 // EXECUTION CODE
 
+// targeting the animation at the image weirdly doesn't work...  is it due to the position absolute or static?
 // hoverAnimateImage1('li.valley_winds div.scene img.component_img');
-hoverAnimateImage1('div.scene img.component_img');
+// hoverAnimateImage1('div.scene img.component_img',tm1);
+hoverAnimateImage1('div.scene div.vlak_0,div.scene div.vlak_1',tm1);
+// hoverAnimateImage1('div.scene div.vlak_0 img,div.scene div.vlak_1 img',tm1);
 
 //////////////////////////////////////////////////
 
