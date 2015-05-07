8a7487348271aabe8aa55f26bac423f7147f295c
Sunny Lam <sunny.lam@gmail.com>
2015-05-06 22:47:31 -0400

    This version has the correct syntax for making the images have a staggered hover effect using TimelineMax and TweenMax. Also created a simple nav bar to practice the animation transitions as we switch from one slider view to another.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,35 @@ jQuery(document).ready(function($) {
 //////////////////////////////////////////////////
 // GLOBAL VARIABLES
 
+// create a new TimelineMax sequencing object
+var tm = new TimelineMax({repeat:-1,yoyo:true});
 
 //////////////////////////////////////////////////
 // FUNCTIONS
 
+function hoverAnimateImage1 (targetE) {
+	// where targetE = the target element, typically a jQuery selector
+	
+	// store a reference to the target
+	var $target = $(targetE);
 
+	tm.yoyo(true);
+	tm.staggerTo($target,2,{css:{transform:'translateZ(5px)'}, ease:Power0.easeOut},1);
+	tm.staggerTo($target,2,{css:{transform:'translateZ(0px)'}, ease:Power0.easeOut},1);
+	// tm.staggerTo($target,0.5,{translateX:5, ease:Power2.easeIn, overwrite:'all'},0.6);
+
+	// tm.to($target,0.5,{rotateX:180, z:-300, ease:Power2.easeIn, overwrite:'all'});
+
+	tm.play();
+
+}
 
 
 //////////////////////////////////////////////////
 // EXECUTION CODE
 
+// hoverAnimateImage1('li.valley_winds div.scene img.component_img');
+hoverAnimateImage1('div.scene img.component_img');
 
 //////////////////////////////////////////////////
 
